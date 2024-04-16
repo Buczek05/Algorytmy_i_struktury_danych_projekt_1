dfddbf746e70af2d0b24afb3a22e857bd3c48023
Kacper Bukowski <kacperbuko2005@gmail.com>
2024-04-17 00:00:54 +0200

    init

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 #include "example_integer.c"
 
 void show_result(struct Integral integral){
-    printf("Using rectangle: %f\n", calculate_integral_using_rectangle(&integral));
-    printf("Using trapezoid: %f\n", calculate_integral_using_trapezoid(&integral));
+//    printf("Using rectangle: %f\n", calculate_integral_using_rectangle(&integral));
+//    printf("Using trapezoid: %f\n", calculate_integral_using_trapezoid(&integral));
     printf("Using monte carlo: %f\n", calculate_integral_using_monte_carlo(&integral));
 }
 
